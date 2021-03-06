483314b394e61a572c746fccef0c35ff3af57089
Jean-Baptiste <Jean-Baptiste.Njifondum@ulb.be>
2021-03-07 00:15:18 +0100

    Pearson residu calculation

--- test_code.m
+++ test_code.m
@@ -3,9 +3,11 @@
 clear all;
 variables;
 %confidence_inter([2000,2018],50)% the person has 50 year in 2000
-date = [1960 1961];
-age = [0 1];
-%[f,fp] = f_alpha(0,k,1, date, age, 20);
-[alp, kap, bet] = leeCar_NR(date, age, 1, 1e-6); 
+date = [1920 2018]; 
+age = [0 109]; %consider ages between [0 109] and dates [1920 2018] We can clearly see a linear trend starting from 1941 for kappa
+%[f,fp] = f_alpha(0,k,1, date, age, 20); 
+[alp, kap, bet] = leeCar_NR(date, age, 600, 1e-6); %if there's problem, plz increase max_iter
 
-%Fkappa gradit trop vite
+%% plotting residual 
+
+[resi, squa] = residual(date, age, alp, bet, kap); %plot year, age etc...
